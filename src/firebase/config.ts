fa8e432aab512c13033fd77fab5e8713af9996a4
Emmanuel Solomon <xostip@gmail.com>
2026-02-15 17:54:23 +0000

    connect this new firebase apikey const firebaseConfig = {   apiKey: "AIz

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,11 @@
 // It's safe to expose this configuration to the client
 // Firebase security rules will handle authentication and authorization
 export const firebaseConfig = {
-  apiKey: "AIzaSyA4NAGaCv3w5TAI7F6dUXkDJV_lFEGElvc",
+  apiKey: "AIzaSyAXNF_LsSgMO7RmQtraARIrMlGElPuG2IY",
   authDomain: "barsales-272a3.firebaseapp.com",
   projectId: "barsales-272a3",
-  storageBucket: "barsales-272a3.appspot.com",
+  storageBucket: "barsales-272a3.firebasestorage.app",
   messagingSenderId: "677933526810",
-  appId: "1:677933526810:web:92ed33ca397f66d65bc6a3",
-  measurementId: "G-ZCH34V0DB7"
+  appId: "1:677933526810:web:a0f6b6b24504abd35bc6a3",
+  measurementId: "G-2Q0JFZB6WH"
 };
