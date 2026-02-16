e3187b25d35246a2c967de1172409998b62056bd
Emmanuel Solomon <xostip@gmail.com>
2026-02-16 07:28:18 +0000

    Try fixing this error: `Console Error: `legacyBehavior` is deprecated an

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -64,15 +64,16 @@ const AdminSidebar = () => {
         <SidebarMenu>
           {navItems.map((item) => (
             <SidebarMenuItem key={item.href}>
-              <Link href={item.href} legacyBehavior passHref>
-                <SidebarMenuButton
-                  isActive={pathname === item.href}
-                  tooltip={item.label}
-                >
+              <SidebarMenuButton
+                asChild
+                isActive={pathname.startsWith(item.href)}
+                tooltip={item.label}
+              >
+                <Link href={item.href}>
                   <item.icon />
                   <span>{item.label}</span>
-                </SidebarMenuButton>
-              </Link>
+                </Link>
+              </SidebarMenuButton>
             </SidebarMenuItem>
           ))}
         </SidebarMenu>
