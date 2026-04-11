e466641cb178df99b1355b59511c1a0251741369
Emmanuel Solomon <xostip@gmail.com>
2026-04-11 11:18:26 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://encrypt

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -31,6 +32,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
         port: '',
         pathname: '/**',
       },
